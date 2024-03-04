a01946190fca4e72a38a4e3a388d54e88f9216c7
pratikjain10 <pratik.jain@blitzscale.co>
2024-03-04 17:36:10 +0530

    testing

---
+++ scripts/readPdf.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const pdf = require("pdf-parse");
+
+exports.readPdf = function (pathToPdf) {
+  return new Promise(function (resolve) {
+    const pdfPath = path.resolve(pathToPdf);
+    let dataBuffer = fs.readFileSync(pdfPath);
+    pdf(dataBuffer).then(function ({ text }) {
+      resolve(text);
+    });
+  });
+};
