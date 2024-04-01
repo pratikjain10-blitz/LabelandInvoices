901340fcbec88faaee1a1585a2e1c0d32ae5bc4d
pratikjain10-blitz <157410627+pratikjain10-blitz@users.noreply.github.com>
2024-04-01 18:16:26 +0530

    Update readPdf.js

--- scripts/readPdf.js
+++ scripts/readPdf.js
@@ -3,12 +3,27 @@ const path = require("path");
 const pdf = require("pdf-parse");
 
 exports.readPdf = function (pathToPdf) {
-  return new Promise(function (resolve) {
+  return new Promise(function (resolve, reject) {
     const pdfPath = path.resolve(pathToPdf);
-    let dataBuffer = fs.readFileSync(pdfPath);
-    pdf(dataBuffer).then(function ({ text }) {
-      resolve(text);
+
+    // Read the PDF file asynchronously
+    fs.readFile(pdfPath, function (err, dataBuffer) {
+      if (err) {
+        // If there's an error reading the file, reject the promise
+        reject(err);
+        return;
+      }
+
+      // Parse the PDF data
+      pdf(dataBuffer)
+        .then(function ({ text }) {
+          // Resolve the promise with the extracted text
+          resolve(text);
+        })
+        .catch(function (err) {
+          // If there's an error parsing the PDF, reject the promise
+          reject(err);
+        });
     });
   });
 };
-
